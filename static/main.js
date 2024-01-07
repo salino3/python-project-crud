743df69f2929a4542c7ad4e4c27e2738245a379d
salino3 <programacionflavio@gmail.com>
2024-01-07 15:52:52 +0100

    fn delete user done

--- static/main.js
+++ static/main.js
@@ -3,6 +3,7 @@ const userForm = document.querySelector("#userForm");
 
 let users = [];
 
+//
 window.addEventListener("DOMContentLoaded", async () => {
   // users = await fetch("/api/users");
   // const data = await users.json();
@@ -71,13 +72,28 @@ function renderUsers(users) {
      <header class="d-flex justify-content-between align-items-center">
       <h3>${user.username}</h3>
       <div>
-       <button class="btn btn-danger btn-sm">delete</button>
-       <button class="btn btn-secondary btn-sm">edit</button>
+       <button class="btn-delete btn btn-danger btn-sm">delete</button>
+       <button class="btn-edit btn btn-secondary btn-sm">edit</button>
       </div>
      </header>
      <p>${user.email}</p>
      <p class="text-truncate">${user.password}</p>
     `;
+
+    const btnDelete = userItem.querySelector('.btn-delete');
+
+    btnDelete.addEventListener('click', async () => {
+       const response = await fetch(`/api/users/${user.id}`, {
+        method: "DELETE"
+       });
+
+       const data = await response.json();
+
+       users = users.filter(user => user.id !== data.id);
+
+       renderUsers(users);
+
+    });
 
     userList.appendChild(userItem);
    });
